5ca4b43a072ac6d13ce3bd11cea62e1fee7f5354
jandev <jandev66@gmail.com>
2025-04-11 15:43:47 +0530

    cart icon added in mobile screen

--- src/components/user/UserHeader.jsx
+++ src/components/user/UserHeader.jsx
@@ -119,8 +119,33 @@ function UserHeader() {
       {/* Mobile Navbar */}
       <MobileNav>
         <MobileNavHeader>
-          <NavbarLogo />
-          <MobileNavToggle isOpen={menuOpen} onClick={() => setMenuOpen(!menuOpen)} />
+          <div className="flex items-center justify-between w-full px-4">
+            <NavbarLogo />
+            <div className="flex items-center gap-4">
+              <Link to="/cart" className="relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                {totalQuantity > 0 && (
+                  <span className="absolute -top-2 -right-2 flex h-4 w-4 items-center justify-center rounded-full bg-red-600 text-[10px] font-bold text-white shadow-md">
+                    {totalQuantity}
+                  </span>
+                )}
+              </Link>
+              <MobileNavToggle isOpen={menuOpen} onClick={() => setMenuOpen(!menuOpen)} />
+            </div>
+          </div>
         </MobileNavHeader>
 
         <MobileNavMenu isOpen={menuOpen}>
